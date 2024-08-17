3fbf541a22fff63b77cab1ea09d33de5d80995c2
Anton <rgeargerge@yandex.ru>
2024-08-17 22:10:44 +0300

    clean

--- api/index.js
+++ api/index.js
@@ -1,23 +1,9 @@
-import catalog from './catalog'
-import company from './company'
-import distributors from './distrubutors'
 import forms from './forms'
-import geoip from './geoip'
-import homepage from './homepage'
 import pages from './pages'
-import press from './press'
-import solutions from './solutions'
 import vars from './vars'
 
 export default {
-  catalog,
-  company,
-  distributors,
   forms,
-  geoip,
-  homepage,
   pages,
-  press,
-  solutions,
   vars,
 }
